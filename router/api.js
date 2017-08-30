830f906f8c2088e894fe5df49d32d0de60365432
GoLok56 <satadii11@gmail.com>
2017-08-31 00:27:13 +0700

    implement get product list

--- router/api.js
+++ router/api.js
@@ -1,5 +1,32 @@
 const express = require('express');
+const request = require('request');
+
+const TOKEN = "de8c73720048c353579e0cb1af71f02c01c77908";
 
 const router = express.Router();
+
+// Get the product list
+router.get("/product", function(req, res){
+  request.get({
+    'url':'http://api.bisatopup.com/product',
+    'headers': {
+      'X-Authorization': TOKEN
+    }
+  },function(error, header, body){
+    if(error) throw error;
+
+    console.log(typeof(body));
+
+    body = JSON.parse(body);
+    body.forEach(product => {
+      product.tambahanField = "Tambah";
+      product.tambahanField2 = "Tambah tambah";
+      product.tambahanField3 = "Tambah tambah tambah";
+    });
+    res.send(body);
+  });
+});
+
+// TODO Implements the rest feature in Evernote's note
 
 module.exports = router;
